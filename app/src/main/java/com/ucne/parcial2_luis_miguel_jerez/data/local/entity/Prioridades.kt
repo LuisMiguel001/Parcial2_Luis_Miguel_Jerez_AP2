c7238f29d2cfd0b90dbec9b14deadfb3a9a62f79
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2024-03-06 18:24:55 -0400

    Agregando la entidad.

---
+++ app/src/main/java/com/ucne/parcial2_luis_miguel_jerez/data/local/entity/Prioridades.kt
@@ -0,0 +1,18 @@
+package com.ucne.parcial2_luis_miguel_jerez.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("table_prioridades")
+data class Prioridades(
+    @PrimaryKey(autoGenerate = true)
+    val idPrioridad : Int = 0,
+    val nombre: String = "",
+    val descripcion: String = "",
+    val plazo: Int = 0,
+    val esNulo: Boolean = false,
+    val Creador: Int = 0,
+    val fechaCreacion: String = "",
+    val modidicador: Int = 0,
+    val fechaModificacion: String = "",
+)
